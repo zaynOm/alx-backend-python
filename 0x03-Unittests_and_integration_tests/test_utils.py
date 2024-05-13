b0e6362ccd37948642075625fb044c65710dff97
zaynom <ouaziz0626@gmail.com>
2024-05-13 19:12:03 +0100

    Task 3: Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 from typing import Any, Dict, Mapping, Sequence
 import unittest
 from unittest.mock import Mock, patch
-import utils
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 
 
@@ -22,7 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
         self, nested_map: Mapping, path: Sequence, expected: Any
     ) -> None:
         """Base test cases"""
-        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
     def test_access_nested_map_exception(
@@ -30,7 +30,7 @@ class TestAccessNestedMap(unittest.TestCase):
     ) -> None:
         """Errors test cases"""
         with self.assertRaises(KeyError):
-            utils.access_nested_map(nested_map, path)
+            access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
@@ -48,5 +48,27 @@ class TestGetJson(unittest.TestCase):
     ):
         """Mock HTTP calls"""
         mock_get_json.return_value.json.return_value = test_payload
-        self.assertEqual(utils.get_json(test_utl), test_payload)
+        self.assertEqual(get_json(test_utl), test_payload)
         mock_get_json.assert_called_once_with(test_utl)
+
+
+class TestMemoize(unittest.TestCase):
+    """Unittest for memoize decorator"""
+
+    def test_memoize(self):
+        """Mock a_method calls"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_a:
+            testcls = TestClass()
+            self.assertEqual(testcls.a_property, 42)
+            self.assertEqual(testcls.a_property, 42)
+            mock_a.assert_called_once()
